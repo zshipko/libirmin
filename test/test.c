3b20415a29102d081a23dc22042fdd6a88aa55ad
zach <zachshipko@gmail.com>
2021-08-26 19:15:33 -0700

    wip

---
+++ test/test.c
@@ -0,0 +1,35 @@
+#include "irmin.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  puts("A");
+  // irmin_init(argv);
+
+  AUTO IrminSchema *schema = irmin_schema_new("irf", NULL, NULL, NULL);
+
+  puts("B");
+  AUTO IrminConfig *config = irmin_config_new(schema);
+
+  puts("C");
+
+  AUTO IrminValue *root = irmin_value_string("wow");
+  assert(irmin_config_set(config, "root", root));
+
+  AUTO IrminRepo *repo = irmin_repo_new(schema, config);
+
+  AUTO Irmin *store = irmin_master(schema, repo);
+
+  char *x = "abc";
+  AUTO IrminValue *a = irmin_value_string(x);
+
+  char *k[] = {"a", "b", "c", NULL};
+  AUTO IrminKey *key = irmin_key(schema, k);
+  assert(irmin_set(store, key, a));
+
+  char *s = irmin_value_to_string(irmin_get(store, key));
+  puts(s);
+
+  return 0;
+}
