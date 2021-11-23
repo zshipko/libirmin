cca5ad57cb0719f11244b3de5914a1a5e2ee958d
zach <zachshipko@gmail.com>
2021-11-23 14:35:21 -0800

    Add rust workflow

--- rust/build.rs
+++ rust/build.rs
@@ -1,21 +1,41 @@
 fn main() {
     let path = std::env::current_dir().unwrap();
 
-    std::process::Command::new("cp")
-        .arg("../libirmin.so")
-        .arg(".")
-        .spawn()
-        .unwrap();
+    let header = if std::path::Path::new("Makefile").exists() {
+        std::process::Command::new("make").spawn().unwrap();
+
+        std::process::Command::new("cp")
+            .arg("./libirmin.so")
+            .arg("rust")
+            .spawn()
+            .unwrap();
+
+        "./irmin.h"
+    } else {
+        std::process::Command::new("make")
+            .arg("-C")
+            .arg("..")
+            .spawn()
+            .unwrap();
+
+        std::process::Command::new("cp")
+            .arg("../libirmin.so")
+            .arg(".")
+            .spawn()
+            .unwrap();
+
+        "../irmin.h"
+    };
 
     println!("cargo:rustc-link-lib=irmin");
     println!("cargo:rustc-link-search=.");
     println!("cargo:rustc-link-arg=-Wl,-rpath,{}", path.display());
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=../irmin.h");
+    println!("cargo:rerun-if-changed={}", header);
 
     let bindings = bindgen::builder()
-        .header("../irmin.h")
+        .header(header)
         .allowlist_type("Irmin.*")
         .allowlist_function("irmin.*")
         .generate()
