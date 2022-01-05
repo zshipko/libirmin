3b2070c917a0c19be3509bfb98b4f59e48e7f4c0
zach <zachshipko@gmail.com>
2022-01-05 15:36:58 -0800

    Make python exports explicit

--- py/irmin/__init__.py
+++ py/irmin/__init__.py
@@ -1,1 +1,5 @@
-from .irmin import *
+from .irmin import IrminException, PathType,  \
+        String, Bytes, error_msg, check,      \
+        Type, Value, Contents, content_types, \
+        log_level, Config, Repo, Hash, Path,  \
+        Info, Commit, Tree, Store
